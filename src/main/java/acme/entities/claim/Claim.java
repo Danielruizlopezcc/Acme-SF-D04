586bb6d0b02d8510c5db75b0709595a6b494aca1
marpadgom1 <marpadgom1@alum.us.es>
2024-03-05 17:44:09 +0100

    Task016 done

---
+++ src/main/java/acme/entities/claim/Claim.java
@@ -0,0 +1,73 @@
+
+package acme.entities.claim;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.client.data.accounts.UserAccount;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Claim extends AbstractEntity {
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Pattern(regexp = "^C-[0-9]{4}$")
+	@Column(unique = true)
+	private String				code;
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				instantiationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				heading;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				description;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				department;
+
+	@Email
+	private String				optionalEmailAddress;
+
+	@URL
+	private String				optionalLink;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private UserAccount			account;
+
+}
