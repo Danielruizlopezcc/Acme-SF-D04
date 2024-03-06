4802682728b5174dd8fd2c7ff14887222acf5009
albcarsic <albcarsic@alum.us.es>
2024-03-06 16:31:37 +0100

    Task054 done

--- src/main/java/acme/roles/Sponsor.java
+++ src/main/java/acme/roles/Sponsor.java
@@ -1,13 +1,20 @@
 
 package acme.roles;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Sponsor extends AbstractRole {
 	// Serialisation identifier -----------------------------------------------
 
@@ -26,7 +33,7 @@ public class Sponsor extends AbstractRole {
 	@URL
 	private String				webPage;
 
-	@URL
+	@Email
 	private String				emailContact;
 
 	// Derived attributes -----------------------------------------------------
