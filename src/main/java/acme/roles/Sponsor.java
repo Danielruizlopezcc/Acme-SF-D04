f96dff4227d950c60552724c5c13b47ade308791
albcarsic <albcarsic@alum.us.es>
2024-03-06 11:42:49 +0100

    Task042 done

---
+++ src/main/java/acme/roles/Sponsor.java
@@ -0,0 +1,35 @@
+
+package acme.roles;
+
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractRole;
+
+public class Sponsor extends AbstractRole {
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 75)
+	private String				name;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				expectedBenefits;
+
+	@URL
+	private String				webPage;
+
+	@URL
+	private String				emailContact;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+}
