f54bca6af8f9a6faa19ecb77a93c0bb4ee26fa52
Rafa <rafpulcif@alum.us.es>
2024-04-04 12:46:38 +0200

    feat: Añadir Repository y CreateService

---
+++ src/main/java/acme/features/developer/DeveloperCreateService.java
@@ -0,0 +1,90 @@
+
+package acme.features.developer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Principal;
+import acme.client.data.models.Dataset;
+import acme.client.helpers.MomentHelper;
+import acme.client.services.AbstractService;
+import acme.entities.project.Project;
+import acme.entities.trainingModule.TrainingModule;
+import acme.roles.Developer;
+
+@Service
+public class DeveloperCreateService extends AbstractService<Developer, TrainingModule> {
+
+	@Autowired
+	private DeveloperRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		TrainingModule object;
+		Developer developer;
+
+		Principal principal = super.getRequest().getPrincipal();
+		developer = this.repository.findOneDeveloperById(principal.getActiveRoleId());
+		object = new TrainingModule();
+		object.setDraftMode(true);
+		object.setDeveloper(developer);
+
+		super.getBuffer().addData(object);
+	}
+
+	@Override
+	public void bind(final TrainingModule object) {
+		assert object != null;
+
+		int projectId;
+		Project project;
+
+		projectId = super.getRequest().getData("project", int.class);
+		project = this.repository.findOneProjectById(projectId);
+
+		super.bind(object, "code", "creationMoment", "details", "difficultyLevel", "updateMoment", "link", "estimatedTotalTime");
+		object.setProject(project);
+
+	}
+
+	@Override
+	public void validate(final TrainingModule object) {
+		assert object != null;
+
+		if (!super.getBuffer().getErrors().hasErrors("code")) {
+			TrainingModule existing;
+
+			existing = this.repository.findOneTMByCode(object.getCode());
+			super.state(existing == null, "code", "developer.training-module.form.error.duplicated");
+		}
+
+		if (!super.getBuffer().getErrors().hasErrors("updateMoment"))
+			super.state(MomentHelper.isAfter(object.getUpdateMoment(), object.getCreationMoment()), "updateMoment", "developer.training-module.form.error.update-date-not-valid");
+	}
+
+	@Override
+	public void perform(final TrainingModule object) {
+		assert object != null;
+
+		this.repository.save(object);
+	}
+
+	@Override
+	public void unbind(final TrainingModule object) {
+		assert object != null;
+
+		Dataset dataset;
+
+		dataset = super.unbind(object, "code", "creationMoment", "details", "difficultyLevel", "updateMoment", "link", "estimatedTotalTime");
+
+		super.getResponse().addData(dataset);
+
+	}
+
+}
