7b28b975f6b455fb58681e33ea09ee6696d81625
albcarsic <albcarsic@alum.us.es>
2024-04-25 18:55:58 +0200

    Task076 done

---
+++ src/main/java/acme/features/authenticated/sponsor/AuthenticatedSponsorController.java
@@ -0,0 +1,25 @@
+
+package acme.features.authenticated.sponsor;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.client.controllers.AbstractController;
+import acme.client.data.accounts.Authenticated;
+import acme.roles.Sponsor;
+
+@Controller
+public class AuthenticatedSponsorController extends AbstractController<Authenticated, Sponsor> {
+
+	@Autowired
+	private AuthenticatedSponsorCreateService createService;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("create", this.createService);
+	}
+
+}
