f854e69d4f05284d0b126166d3cea2757dd67571
Ramón Gavira <ramgavsan@alum.us.es>
2024-05-23 17:52:28 +0200

    Revisión en clase y system conf

--- src/main/java/acme/entities/progressLogs/ProgressLog.java
+++ src/main/java/acme/entities/progressLogs/ProgressLog.java
@@ -5,7 +5,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -25,6 +27,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "contract_id")
+})
 public class ProgressLog extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
